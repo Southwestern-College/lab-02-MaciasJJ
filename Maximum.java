d4f1a01bc7ad9efa0593d1be0e9db3908673b6ce
MaciasJJ <jm1143035@swccd.edu>
2025-09-17 01:16:53 +0000

    Finalized Maximum.java program

--- Maximum.java
+++ Maximum.java
@@ -5,10 +5,10 @@
  */
 
 import java.util.Scanner;
-
 public class Maximum {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        
 //Prompt the user to enter values for variables number1, number2, and number3.
         System.out.println("Please enter a value for variable 1");
         int number1 = input.nextInt();
@@ -18,20 +18,20 @@ public class Maximum {
         
         System.out.println("Please enter a value for variable 3");
         int number3 = input.nextInt();
+    int max;
+//Calculate the maximum value
+        max = number1;
+        if (max <= number2) {
+            max = number2;
+        }
+        if (max <= number3) {
+            max = number3;
+        }
+        
+        //Output maximum value
+        System.out.println("The maximum value is: " + max);
 
-//Display the maximum
-        System.out.println("The max is:");
-        if (number1 >= number2)
-            System.out.println(number1);
-        if (number1 >= number3)
-            System.out.println(number1);
-        if (number2 >= number1)
-            System.out.println(number2);
-        if (number2 >= number3)
-            System.out.println(number2);
-        if (number3 >= number1)
-            System.out.println(number3);
-        if (number3 >= number2)
-            System.out.println(number3);
+        //Close the program
+        input.close();
     }
 }
